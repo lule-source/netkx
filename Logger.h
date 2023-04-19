d6384f4c49f45f85af1762e63eda423eb1c377a7
刘磊 <lei.liu@kanjian.com>
2023-04-19 15:22:06 +0800

    fix log

--- Logger.h
+++ Logger.h
@@ -3,46 +3,46 @@
 #include <string>
 #include "noncopyable.h"
 
-#define LOG_INFO(logmsgFormat, ...)                      \
-    do                                                   \
-    {                                                    \
-        Logger &logger = Logger::instance();             \
-        logger.setLogLevel(INFO);                        \
-        char buf[1024] = {0};                            \
+#define LOG_INFO(logmsgFormat, ...) \
+    do \
+    { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(INFO); \
+        char buf[1024] = {0}; \
         snprintf(buf, 1024, logmsgFormat, ##_VA_ARGS__); \
-        logger.log(buf);                                 \
+        logger.log(buf); \
     } while (0)
 
-#define LOG_ERROR(logmsgFormat, ...)                      \
-    do                                                    \
-    {                                                     \
-        Logger &logger = Logger::instance();              \
-        logger.setLogLevel(ERROR);                        \
-        char buf[1024] = {0};                             \
+#define LOG_ERROR(logmsgFormat, ...) \
+    do \
+    { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(ERROR); \
+        char buf[1024] = {0}; \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf);                                  \
+        logger.log(buf); \
     } while (0)
 
-#define LOG_FATAL(logmsgFormat, ...)                      \
-    do                                                    \
-    {                                                     \
-        Logger &logger = Logger::instance();              \
-        logger.setLogLevel(FATAL);                        \
-        char buf[1024] = {0};                             \
+#define LOG_FATAL(logmsgFormat, ...) \
+    do \
+    { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(FATAL); \
+        char buf[1024] = {0}; \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf);                                  \
-        exit(-1);                                         \
+        logger.log(buf); \
+        exit(-1); \
     } while (0)
 
 #ifdef MUDEBUG
-#define LOG_DEBUG(logmsgFormat, ...)                      \
-    do                                                    \
-    {                                                     \
-        Logger &logger = Logger::instance();              \
-        logger.setLogLevel(DEBUG);                        \
-        char buf[1024] = {0};                             \
+#define LOG_DEBUG(logmsgFormat, ...) \
+    do \
+    { \
+        Logger &logger = Logger::instance(); \
+        logger.setLogLevel(DEBUG); \
+        char buf[1024] = {0}; \
         snprintf(buf, 1024, logmsgFormat, ##__VA_ARGS__); \
-        logger.log(buf);                                  \
+        logger.log(buf); \
     } while (0)
 #else
 #define LOG_DEBUG(logmsgFormat, ...)
